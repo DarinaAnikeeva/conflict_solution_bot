31b8b216286a564d6dc1fca6ecef080368d55d04
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2024-12-09 11:31:08 +0300

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Situation
+from .models import Situation, Advice, Feedback
 
 @admin.register(Situation)
 class SituationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Advice)
+class AdviceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
     pass
